92438e40c18fa684abbe28653b13f5ab5a324460
thislooksfun <thislooksfun@repbot.org>
2015-02-05 10:52:20 -0600

    Stuff

--- js/form.js
+++ js/form.js
@@ -1,12 +1,39 @@
 function selectForm(form)
 {
-	$("#select").css("left", "-100%");
+	//$("#select").css("left", "-100%");
 	$(form).css("left", "0px");
 }
 
 function back()
 {
-	$("#select").css("left", "0px");
+//	$("#select").css("left", "0px");
 	$("#bug").css("left", "100%");
 	$("#suggestion").css("left", "100%");
+}
+
+function submitBug()
+{
+	var bugForm = $('#bugForm');
+	/*$.ajax({
+		type: "POST",
+		url: bugForm.attr('action'),
+		data: bugForm.serialize(),
+		success: function(response) {
+        	console.log(response);
+      	}
+	});*/
+	return false;
+}
+function submitSuggestion()
+{
+	var suggestionForm = $('#suggestionForm');
+	/*$.ajax({
+		type: "POST",
+		url: suggestionForm.attr('action'),
+		data: suggestionForm.serialize(),
+		success: function(response) {
+        	console.log(response);
+      	}
+	});*/
+	return false;
 }
